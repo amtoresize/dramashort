84f66e9267040aa7496eb65ed5cf084567889fd3
amtoresize <mediameita@gmail.com>
2026-01-18 22:24:35 +0700

    Update player.js

--- functions/player.js
+++ functions/player.js
@@ -1,14 +1,15 @@
-﻿export async function onRequest(context) {
+export async function onRequest(context) {
   const { request } = context;
   const url = new URL(request.url);
-  const videoUrl = url.searchParams.get("url");
+  const videoUrl = url.searchParams.get('url');
   
   if (!videoUrl) {
-    return new Response("Missing video URL", { status: 400 });
+    return new Response('Missing video URL', { status: 400 });
   }
   
-  const PLRJS_BASE = "https://cdn.plrjs.com/player/bsa9hkrkynde7/qqvmenqpoeve.html";
-  const playerUrl = \`\${PLRJS_BASE}?file=\${encodeURIComponent(videoUrl)}\`;
+  // PLRJS Player Anda
+  const PLRJS_BASE = 'https://cdn.plrjs.com/player/bsa9hkrkynde7/qqvmenqpoeve.html';
+  const playerUrl = PLRJS_BASE + '?file=' + encodeURIComponent(videoUrl);
   
   return Response.redirect(playerUrl, 302);
 }
